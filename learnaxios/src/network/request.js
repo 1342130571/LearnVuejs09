313b4169414289ccdd9a7d3e48dbb77db29b6e8d
1342130571 <121421857+1342130571@users.noreply.github.com>
2023-08-21 15:47:14 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   learnaxios/src/network/request.js

--- learnaxios/src/network/request.js
+++ learnaxios/src/network/request.js
@@ -42,7 +42,30 @@ export function request(config) {
     baseURL: "http://123.207.32.32:8000",
     timeout: 5000
   })
+  
+  // 2.axios的拦截器
+  // 2.1.请求拦截的作用
+  instance.interceptors.request.use(config => {
+    console.log(config)
+    // 1.比如config中的一些信息不符合服务器的要求
 
-  // 创建真正的网络请求
+    // 2.比如每次发送网络请求时，都希望在界面中显示一个请求图标
+
+    // 3.某些网络请求(比如登录(token))，必须携带一些特殊的信息
+    return config
+  }, err => {
+    console.log(err);
+  })
+
+  // 3.2.响应拦截
+  instance.interceptors.response.use(res => {
+    console.log(res);
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+
+  // 3.创建真正的网络请求
   return instance(config)
 }
